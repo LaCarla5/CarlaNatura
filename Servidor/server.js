830f65492748960a8dcb120d921e5754631436c5
LaCarla5 <carlarnedo2004@gmail.com>
2026-05-15 22:20:36 +0200

    CambioServer

--- Servidor/server.js
+++ Servidor/server.js
@@ -20,7 +20,7 @@ const axios = require('axios');
 const app = express();
 
 app.use(cors({
-  origin: 'https://carla-natura.vercel.app', // Pega aquí TU URL real
+  origin: 'https://carlanatura.onrender.com', // Pega aquí TU URL real
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
