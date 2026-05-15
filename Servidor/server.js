226a4a21a60d152f0710d42685de2c6503fc9263
LaCarla5 <carlarnedo2004@gmail.com>
2026-05-15 22:35:28 +0200

    CambiosConexion

--- Servidor/server.js
+++ Servidor/server.js
@@ -58,12 +58,15 @@ const universalStorage = multer.diskStorage({
 const upload = multer({ storage: universalStorage });
 
 // Conexión MySQL
-const conexion = mysql.createConnection({
+const conexion = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 conexion.connect((err) => {
